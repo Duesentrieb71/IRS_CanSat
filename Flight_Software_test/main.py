4c9259e1f444be596ae26f48e7c9e741da5b9d02
daniel.philipp <daniel.philipp@ksat-stuttgart.de>
2023-06-21 22:32:33 +0200

    added timeout

--- Flight_Software_test/main.py
+++ Flight_Software_test/main.py
@@ -1,16 +1,30 @@
 import comms
 import sensor_data
+
 import uasyncio # Using async from MicroPython
 
-released = False
+# Nach der CanSat_timeout Zeit wird die Datenaufzeichnung und das Empfangen des Signals beendet
+CanSat_timeout = 300 # Sekunden
 
+# Funktion zur Datenaufzeichnung und zum Empfangen des Signals
 async def main():
+    # Das Programm wartet auf das Drücken des Startknopfes
+    task_button_press_start = uasyncio.create_task(sensor_data.button_press_start())
+    await uasyncio.gather(task_button_press_start)
+
     task_get_status = uasyncio.create_task(comms.get_status())
-    task_get_data = uasyncio.create_task(sensor_data.main())
-    await uasyncio.gather(task_get_status, task_get_data)
+    task_get_data = uasyncio.create_task(sensor_data.collect_data())
+
+    try:
+        await uasyncio.wait_for(uasyncio.gather(task_get_status, task_get_data), timeout=CanSat_timeout)
+    except uasyncio.TimeoutError:
+        print("Timeout! Exiting...")
+        task_get_status.cancel()
+        task_get_data.cancel()
 
     print("done")
 
 
 if __name__ == "__main__":
+
     uasyncio.run(main())
