a444bf61e63563dc0a9c03bb7c28bc80df7827b7
Daniel Philipp <daniel.philipp@ksat-stuttgart.de>
2023-08-23 12:56:39 +0200

    added release and quiz folder

---
+++ Flight_Software_quiz/main.py
@@ -0,0 +1,46 @@
+import comms
+import sensor_data
+import actuator
+import uasyncio # Using async from MicroPython
+import machine
+import time
+
+# Funktion zur Datenaufzeichnung und zum Empfangen des Funk-Signals
+async def services():
+    # Das Programm wartet auf das Drücken des Knopfes zum Starten
+    task_button_press = uasyncio.create_task(sensor_data.button_press())
+    await uasyncio.gather(task_button_press)
+    time.sleep(0.5)
+    comms.switch_esp32_command() # Schaltet die Kamera ein
+
+    # Es werden drei Tasks erstellt, die gleichzeitig ausgeführt werden
+    task_get_status = uasyncio.create_task(comms.get_receiver_status()) # Empfangen des Signals
+    task_get_data = uasyncio.create_task(sensor_data.collect_data()) # Datenaufzeichnung
+    task_check_esp32_status = uasyncio.create_task(comms.check_esp32_status()) # Überprüfen des Status des ESP32
+    task_button_press = uasyncio.create_task(sensor_data.button_press()) # Knopfdruck zum Beenden
+
+    # Das Programm wartet auf das Drücken des Knopfes zum Beenden der Datenaufzeichnung und des Empfangens des Funk-Signals
+    await uasyncio.gather(task_button_press)
+    sensor_data.csv.close() # Schließt die csv-Datei
+    task_get_status.cancel()
+    task_get_data.cancel()
+    task_check_esp32_status.cancel()
+    comms.switch_esp32_command() # Schaltet die Kamera aus
+    actuator.reset_status() # Setzt den Status der Sensoren zurück
+    await uasyncio.sleep(0.5)
+    # Pi Pico neustarten
+    machine.reset()
+
+
+async def main():
+    task_services = uasyncio.create_task(services())
+    task_update_LED = uasyncio.create_task(actuator.update_LED())
+    await uasyncio.gather(task_services, task_update_LED)
+
+if __name__ == "__main__":
+    uasyncio.run(main())
+
+
+
+# TODO: look into interrupts for the button press
+# TODO: (maybe) implement UART communication between the ESP32 and the pico
---
+++ Flight_Software_release/actuator.py
@@ -0,0 +1,84 @@
+#responsible for controlling the actuators which release the payload and the parachute
+from machine import Pin
+import time
+import uasyncio # Using async from MicroPython
+import comms
+import sensor_data
+import neopixel
+
+# Motor H-Brücke (GP16/GP17)
+motor_1 = Pin(16, Pin.OUT)
+motor_2 = Pin(17, Pin.OUT)
+motor_status = False
+
+async def Motor_H_Bridge(direction):
+    global motor_status
+    if direction == 1:
+        motor_1.value(1)
+        motor_2.value(0)
+        motor_status = True
+    elif direction == 2:
+        motor_1.value(0)
+        motor_2.value(1)
+        motor_status = True
+    elif direction == 0:
+        motor_1.value(0)
+        motor_2.value(0)
+        motor_status = False
+
+
+
+
+# sdcard_status
+# accel_status
+# gyro_status
+# pressure_status
+# temperature_status
+# write_data_status
+# esp32_status
+# esp32_command
+# receiver_status
+
+total_status = False
+
+np = neopixel.NeoPixel(Pin(12), 1)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+orange = (255, 165, 0)
+violett = (238, 130, 238)
+
+
+async def update_LED():
+    while True:
+        if comms.esp32_command and comms.esp32_status and comms.receiver_status and sensor_data.sdcard_status and sensor_data.accel_status and sensor_data.gyro_status and sensor_data.pressure_status and sensor_data.temperature_status and sensor_data.write_data_status: # indicates fully operational
+            np[0] = green
+            np.write()
+        elif not comms.esp32_command and not comms.esp32_status and not comms.receiver_status and sensor_data.sdcard_status and not sensor_data.accel_status and not sensor_data.gyro_status and not sensor_data.pressure_status and not sensor_data.temperature_status and not sensor_data.write_data_status: # indicates planned standby
+            np[0] = blue
+            np.write()
+        elif comms.esp32_command and not comms.esp32_status and comms.receiver_status and sensor_data.sdcard_status and sensor_data.accel_status and sensor_data.gyro_status and sensor_data.pressure_status and sensor_data.temperature_status and sensor_data.write_data_status:
+            np[0] = orange # Everything is fine except the ESP32-CAM
+            # print("ESP32-CAM not responding")
+            np.write()
+        elif comms.esp32_command and comms.esp32_status and not comms.receiver_status and sensor_data.sdcard_status and sensor_data.accel_status and sensor_data.gyro_status and sensor_data.pressure_status and sensor_data.temperature_status and sensor_data.write_data_status:
+            np[0] = violett # Everything is fine except the receiver
+            # print("Sensors not responding")
+            np.write()
+        else:
+            np[0] = red
+            np.write()
+
+        # print("esp32_command: {} | esp32: {} | receiver: {} | sdcard: {} | accel: {} | gyro: {} | pressure: {} | temperature: {} | write_data: {}".format(comms.esp32_command, comms.esp32_status, comms.receiver_status, sensor_data.sdcard_status, sensor_data.accel_status, sensor_data.gyro_status, sensor_data.pressure_status, sensor_data.temperature_status, sensor_data.write_data_status))
+
+        await uasyncio.sleep(0.5)
+
+def reset_status(): # currently not used
+    comms.esp32_status = False
+    comms.receiver_status = False
+    sensor_data.sdcard_status = False
+    sensor_data.accel_status = False
+    sensor_data.gyro_status = False
+    sensor_data.pressure_status = False
+    sensor_data.temperature_status = False
+    sensor_data.write_data_status = False
